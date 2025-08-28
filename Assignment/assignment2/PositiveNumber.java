db1c130d88b4f510cd8076f195a17200994855e8
samruddhi paramane <smaruddhiparamane21@gmail.com>
2025-08-28 11:23:30 +0530

    add

---
+++ Assignment/assignment2/PositiveNumber.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class PositiveNumber{
+public static void main(String[] args){
+int n;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter a number:");
+n=sc.nextInt();
+if(n>0)
+System.out.println("number is positive");
+}
+}
